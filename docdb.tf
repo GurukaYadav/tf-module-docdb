bdf6c2bf8dc636a0e81156480abae5e9e7aa32c5
Avinash Yadav <avinash.guruka@gmail.com>
2023-02-28 14:14:14 +0530

    updating tf-module-docdb

--- docdb.tf
+++ docdb.tf
@@ -2,8 +2,8 @@ resource "aws_docdb_cluster" "docdb" {
   cluster_identifier = "${local.TAG_NAME}-docdb-cluster"
   engine             =  var.DOC_DB_ENGINE
   engine_version     =  var.DOC_DB_ENGINE_VERSION
-  master_username    = jsondecode(data.aws_secretsmanager_secret_version.example.secret_string)["DOCDB_USER"]
-  master_password    = jsondecode(data.aws_secretsmanager_secret_version.example.secret_string)["DOCDB_PASS"]
+  master_username    = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_USER"]
+  master_password    = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_PASS"]
   db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.pg.name
   db_subnet_group_name = aws_docdb_subnet_group.docdb.name
   vpc_security_group_ids = [aws_security_group.docdb.id]
