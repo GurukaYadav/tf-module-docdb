9dbb1b2a3d57d4044727e05a9becc61ce687cb7a
Avinash Yadav <avinash.guruka@gmail.com>
2023-03-04 21:36:42 +0530

    updating tf-module-docdb

--- schema.tf
+++ schema.tf
@@ -5,8 +5,8 @@ curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongo
 cd /tmp
 unzip -o mongodb.zip
 cd mongodb-main
-mongo --tls --host ${aws_docdb_cluster.docdb.endpoint}:27017 --tlsCAFile /root/rds-combined-ca-bundle.pem  --username ${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_USER"]} --password ${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_PASS"]} < catalogue.js
-mongo --tls --host ${aws_docdb_cluster.docdb.endpoint}:27017 --tlsCAFile /root/rds-combined-ca-bundle.pem  --username ${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_USER"]} --password ${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_PASS"]} < users.js
+mongo --tls --host ${aws_docdb_cluster.docdb.endpoint}:27017 --tlsCAFile /home/centos/rds-combined-ca-bundle.pem  --username ${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_USER"]} --password ${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_PASS"]} < catalogue.js
+mongo --tls --host ${aws_docdb_cluster.docdb.endpoint}:27017 --tlsCAFile /home/centos/rds-combined-ca-bundle.pem  --username ${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_USER"]} --password ${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_PASS"]} < users.js
 EOF
   }
 }
